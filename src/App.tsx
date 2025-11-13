4e97841956364685eafe341eec6f972865426ee9
LETUSOWNER <amansinghbillion@gmail.com>
2025-11-14 00:03:09 +0530

    fix: tsconfig and import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { JSX, useEffect, useMemo, useState } from "react";
 
 /* ============================
    Constants & Types (unchanged)
